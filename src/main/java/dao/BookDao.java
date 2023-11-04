7f0d2020086d5317577d7e945b7dada314f515ad
Mercy <d00251752@student.dkit.ie>
2023-11-04 08:41:50 +0000

    Create getAllBook method in BookDao.

--- src/main/java/dao/BookDao.java
+++ src/main/java/dao/BookDao.java
@@ -3,10 +3,8 @@ package dao;
 import business.Book;
 import exceptions.DaoException;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-import java.sql.SQLIntegrityConstraintViolationException;
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class BookDao extends Dao implements BookDaoInterface  {
@@ -64,7 +62,42 @@ public class BookDao extends Dao implements BookDaoInterface  {
 
     @Override
     public List<Book> getAllBook() throws DaoException {
-        return null;
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        List<Book> products = new ArrayList<Book>();
+
+        try{
+            con = getConnection();
+
+            String query = "Select * from book";
+            ps = con.prepareStatement(query);
+            rs = ps.executeQuery();
+
+            while(rs.next())
+            {
+                Book b = new Book(rs.getInt("id"), rs.getString("title"), rs.getString("author"), rs.getInt("ISBN"), rs.getDate("publication_date"), rs.getInt("qty"), rs.getString("description"), rs.getInt("copy_qty"));
+                products.add(b);
+            }
+        }catch (SQLException e) {
+            System.out.println("Exception occured in the getAllBook() method: " + e.getMessage());
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.out.println("Exception occured in the finally section of the getAllBook() method: " + e.getMessage());
+            }
+        }
+
+        return products;
     }
 
     @Override
