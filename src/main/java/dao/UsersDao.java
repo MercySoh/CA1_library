847ba12cf3ee458e7b54671120f9bee0a788d939
cgarvey00 <conorgarvey19@outlook.com>
2023-11-04 20:29:46 +0000

    The Updated UserDao class

--- src/main/java/dao/UsersDao.java
+++ src/main/java/dao/UsersDao.java
@@ -1,14 +1,17 @@
 package dao;
 
 import business.Users;
+import exceptions.DuplicationEmailException;
+import exceptions.DuplicationUsernameException;
+import org.mindrot.jbcrypt.BCrypt;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * @author Julie, Conor
+ */
 public class UsersDao extends Dao implements UsersDaoInterface {
 
     public UsersDao(String dbName) {
@@ -74,13 +77,6 @@ public class UsersDao extends Dao implements UsersDaoInterface {
         return usersList;
     }
 
-    /**
-     * This method will allow the search for a user using a
-     * user_id variable
-     *
-     * @param user_id the user ID supplied of the individual
-     * @return A User Object containing the user_id
-     */
     @Override
     public Users findUserByUserID(int user_id) {
         Connection con = null;
@@ -125,28 +121,250 @@ public class UsersDao extends Dao implements UsersDaoInterface {
 
     @Override
     public int userRegister(String name, String email, String username, String password, String phone, String address, String city, String postcode) {
-        return 0;
+
+        if (emailCheck(email)) {
+            throw new DuplicationEmailException("The Email Already exists");
+        }
+        if (usernameCheck(username)) {
+            throw new DuplicationUsernameException("The username already exists");
+        }
+
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int newId = -1;
+
+        String hashPassword = BCrypt.hashpw(password, BCrypt.gensalt());
+        try {
+            con = this.getConnection();
+
+            String query = "INSERT INTO users(name, email, username, password,phone,address,city,postcode) VALUES (?, ?, ?, ?,?,?,?,?)";
+
+            ps = con.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+
+            ps.setString(1, name);
+            ps.setString(2, email);
+            ps.setString(3, username);
+            ps.setString(4, hashPassword);
+            ps.setString(5, phone);
+            ps.setString(6, address);
+            ps.setString(7, city);
+            ps.setString(8, postcode);
+
+            ps.executeUpdate();
+
+            rs = ps.getGeneratedKeys();
+
+            if (rs.next()) {
+
+                newId = rs.getInt(1);
+
+            }
+        } catch (SQLException e) {
+            System.err.println("\tA problem occurred during the addStockItem method:");
+            System.err.println("\t" + e.getMessage());
+            newId = -1;
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (ps != null) {
+                    ps.close();
+                }
+                if (con != null) {
+                    freeConnection(con);
+                }
+            } catch (SQLException e) {
+                System.err.println("A problem occurred when closing down the addStockItem method:\n" + e.getMessage());
+            }
+        }
+        return newId;
     }
 
     @Override
     public Users checkLogin(String email, String password) {
-        return null;
+        Users u = null;
+        if (!emailCheck(email)) {
+
+            Connection con = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+
+            try {
+                con = this.getConnection();
+
+                String query = "SELECT * FROM users where email=?";
+
+                ps = con.prepareStatement(query);
+
+                rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    String hashPassword = rs.getString("password");
+                    boolean isPresent = BCrypt.checkpw(password, hashPassword);
+                    if (isPresent) {
+
+                        int id = rs.getInt("id");
+                        String name = rs.getString("name");
+                        String username = rs.getString("username");
+                        String storedEmail = rs.getString("email");
+                        String address = rs.getString("address");
+                        String phone = rs.getString("phone");
+                        String city = rs.getString("city");
+                        String postcode = rs.getString("postcode");
+                        int disabled = rs.getInt("disable");
+                        int user_type = rs.getInt("user-type");
+
+                        u = new Users(id, name, storedEmail, username, null, phone, address, city, postcode, user_type, disabled);
+
+                    } else {
+                        u = null;
+                    }
+
+                }
+            }catch (SQLException e) {
+                System.err.println("\tA problem occurred during the checkLogin method:");
+                System.err.println("\t" + e.getMessage());
+            } finally {
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                    if (ps != null) {
+                        ps.close();
+                    }
+                    if (con != null) {
+                        freeConnection(con);
+                    }
+                } catch (SQLException e) {
+                    System.err.println("A problem occurred when closing down the checkLogin method:\n" + e.getMessage());
+                }
+            }
+        }
+            return u;
+        }
+
+        @Override
+        public boolean usernameCheck(String username) {
+            boolean isPresent = false;
+            Connection con = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try {
+                con = this.getConnection();
+                String query = "SELECT * from users where username=? ";
+                ps = con.prepareStatement(query);
+                ps.setString(1, username);
+
+                rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    int count=rs.getInt(1);
+                    isPresent =count>0;
+                }
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                System.out.println("	A problem occurred during the duplicateUsername method:");
+            } finally {
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                    if (ps != null) {
+                        ps.close();
+                    }
+                    if (con != null) {
+                        freeConnection(con);
+                    }
+                } catch (SQLException e) {
+                    System.err.println("A problem occurred when closing down the duplicateUsername() method");
+                    System.out.println(e.getMessage());
+                }
+            }
+            return isPresent;
+        }
+
+        @Override
+        public boolean emailCheck(String email) {
+            boolean isPresent = false;
+            Connection con = null;
+            PreparedStatement ps = null;
+            ResultSet rs = null;
+            try {
+                con = this.getConnection();
+                String query = "SELECT * from users where email=? ";
+                ps = con.prepareStatement(query);
+                ps.setString(1, email);
+
+                rs = ps.executeQuery();
+
+                if (rs.next()) {
+                    int count=rs.getInt(1);
+                    isPresent =count>0;
+
+                }
+
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+                System.out.println("	A problem occurred during the duplicateEmail method:");
+            } finally {
+                try {
+                    if (rs != null) {
+                        rs.close();
+                    }
+                    if (ps != null) {
+                        ps.close();
+                    }
+                    if (con != null) {
+                        freeConnection(con);
+                    }
+                } catch (SQLException e) {
+                    System.err.println("A problem occurred when closing down the duplicateEmail() method");
+                    System.out.println(e.getMessage());
+                }
+            }
+            return isPresent;
+        }
+
+        @Override
+        public int deleteUser(int user_id) {
+            Connection con = null;
+            PreparedStatement ps = null;
+            int rows = 0;
+
+            try {
+                con = this.getConnection();
+
+                String query = "DELETE FROM users WHERE user_id = ?";
+                ps = con.prepareStatement(query);
+                ps.setInt(1, user_id);
+
+                rows = ps.executeUpdate();
+            } catch (SQLException e) {
+                System.err.println("\tA problem occurred during the deleteUser method:");
+                System.err.println("\t" + e.getMessage());
+            } finally {
+                try {
+                    if (ps != null) {
+                        ps.close();
+                    }
+                    if (con != null) {
+                        freeConnection(con);
+                    }
+                } catch (SQLException e) {
+                    System.err.println("A problem occurred when closing down the deleteUser method:\n" + e.getMessage());
+                }
+            }
+
+            if(rows==0){
+                System.err.println("No user_id :" + user_id+ "found");
+
+            }
+
+            return rows;
+        }
+
+
     }
-
-    @Override
-    public boolean usernameCheck(String username) {
-        return false;
-    }
-
-    @Override
-    public boolean emailCheck(String email) {
-        return false;
-    }
-
-    @Override
-    public int deleteUser(int user_id) {
-        return 0;
-    }
-
-
-}
