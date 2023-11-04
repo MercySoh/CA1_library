3f448b2d70748e7d50952d7f08e9d01d534e5005
cgarvey00 <conorgarvey19@outlook.com>
2023-11-04 14:37:58 +0000

    Creating exceptions for registering the User, by checking for any existing email and username

---
+++ src/main/java/exceptions/DuplicationEmailException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class DuplicationEmailException extends Exception{
+    public DuplicationEmailException(String m){
+        super(m);
+    }
+}
---
+++ src/main/java/exceptions/DuplicationUsernameException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class DuplicationUsernameException extends Exception {
+    public DuplicationUsernameException(String m){
+        super(m);
+    }
+}
