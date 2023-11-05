a5d9605f311685e5711a905700e864df08109d44
Mercy <d00251752@student.dkit.ie>
2023-11-05 20:02:18 +0000

    Create BookDaoTest class.

---
+++ src/test/java/test/BookDaoTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookDaoTest {
+
+    @Test
+    void addBook() {
+    }
+
+    @Test
+    void getAllBook() {
+    }
+
+    @Test
+    void getBookByTitle() {
+    }
+
+    @Test
+    void increaseCopyStock() {
+    }
+
+    @Test
+    void decreaseCopyStock() {
+    }
+
+    @Test
+    void deleteBook() {
+    }
+
+    @Test
+    void getBookById() {
+    }
+}
