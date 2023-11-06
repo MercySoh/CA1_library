e5168e716152c1bde266c3d33e79e015f0fad710
cgarvey00 <conorgarvey19@outlook.com>
2023-11-06 01:25:34 +0000

    Updating the BorrowDao Test File

--- src/test/java/dao/BorrowDaoTest.java
+++ src/test/java/dao/BorrowDaoTest.java
@@ -14,45 +14,172 @@ public class BorrowDaoTest {
 
 
     /**
+     * This tests deleteBorrow1
+     * where it is under normal conditions
+     */
+    @Test
+    void deleteBorrow1() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+
+        borrowDao.bookBorrow(2, 3);
+        borrowDao.returnBook(2,3);
+
+        //Rows affected
+        assertEquals(1, borrowDao.deleteLoan(2,3));
+    }
+
+    /**
+     * This tests deleteBorrow1
+     * where it is under abnormal conditions
+     * where the values are not in the database
+     */
+    @Test
+    void deleteBorrow2() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+
+
+        assertEquals(1, borrowDao.deleteLoan(200,300));
+    }
+
+
+    /**
      * This tests borrowing the book with no chance of failure
      * Run into problems during the borrowBook test.
      */
     @Test
     void bookBorrowTest1() throws DaoException {
-         BorrowDao borrowDao = new BorrowDao("testlibrary");
-            int user_id = 2;
-            int book_id = 1;
-       int actual=  borrowDao.bookBorrow(user_id,book_id);
-            assertEquals(1, actual);
-
-            // Verify that the user has the expected current loan
-            List<Borrow> actualBorrow = borrowDao.getAllCurrentLoansByUserID(user_id);
-
-            // Prepare the expected Borrow object
-            List<Borrow> expectedBorrow = new ArrayList<>();
-            expectedBorrow.add(new Borrow(1, user_id, book_id, LocalDate.now(), LocalDate.now().plusWeeks(4), null, 0));
-
-            assertEquals(expectedBorrow, actualBorrow);
-        }
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+        int user_id = 2;
+        int book_id = 1;
+        int actual = borrowDao.bookBorrow(user_id, book_id);
+        assertEquals(1, actual);
 
 
-    @Test
-    void returnBook() {
+        List<Borrow> actualBorrow = borrowDao.getAllCurrentLoansByUserID(user_id);
+
+        List<Borrow> expectedBorrow = new ArrayList<>();
+        expectedBorrow.add(new Borrow(1, user_id, book_id, LocalDate.now(), LocalDate.now().plusWeeks(4), null, 0));
+
+        // deleting the loan for the next test
+        borrowDao.deleteLoan(2,1);
+        assertEquals(expectedBorrow, actualBorrow);
     }
 
+    /**
+     * This bookBorrowTest2 borrowing the book with failure
+     * occurring, with a duplicate book
+     */
     @Test
-    void getAllCurrentLoansByUserID() {
+    void bookBorrowTest2() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+
+        // The same book being borrowed three times
+        borrowDao.bookBorrow(1, 1);
+        borrowDao.bookBorrow(1, 1);
+        borrowDao.bookBorrow(1, 1);
+
+        List<Borrow> actual = borrowDao.getAllCurrentLoansByUserID(1);
+
+        List<Borrow> expectedBorrow = new ArrayList<>();
+        expectedBorrow.add(new Borrow(1, 1, 1, LocalDate.now(), LocalDate.now().plusWeeks(4), null, 0));
+        // deleting the loan for the next test
+        borrowDao.deleteLoan(1,1);
+        assertEquals(expectedBorrow, actual);
     }
 
+
+    /**
+     * This returnBook_test1 returning the book under
+     * normal conditions
+     */
     @Test
-    void getAllLoansByUserID() {
+    void returnBook_test1() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+        borrowDao.bookBorrow(2,1);
+        int user_id = 2;
+        int book_id = 1;
+        int actual = borrowDao.returnBook(user_id, book_id);
+        assertEquals(1, actual);
+
+
+        List<Borrow> actualReturn = borrowDao.getAllCurrentLoansByUserID(user_id);
+
+        List<Borrow> expectedBorrow = new ArrayList<>();
+        expectedBorrow.add(new Borrow(1, user_id, book_id, LocalDate.now(), LocalDate.now().plusWeeks(4), null, 0));
+        // deleting the loan for the next test
+        borrowDao.deleteLoan(2,1);
+        assertEquals(expectedBorrow, actualReturn);
     }
 
+    /**
+     * This tests getAllCurrentLoansByUserID
+     * where it is under normal conditions
+     */
     @Test
-    void checkingDuplicateLoans() {
+    void getAllCurrentLoansByUserID() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+        int user_id = 3;
+        int book_id = 2;
+        borrowDao.bookBorrow(user_id, book_id);
+
+        // Getting expected Borrowed Books
+        List<Borrow> actualBorrow = borrowDao.getAllCurrentLoansByUserID(user_id);
+
+        List<Borrow> expectedBorrow = new ArrayList<>();
+        expectedBorrow.add(new Borrow(1, user_id, book_id, LocalDate.now(), LocalDate.now().plusWeeks(4), null, 0));
+        // deleting the loan for the next test
+        borrowDao.deleteLoan(3,2);
+        assertEquals(expectedBorrow, actualBorrow);
     }
 
+    /**
+     * This tests getAllCurrentLoansByUserID
+     * where it is under abnormal conditions
+     */
     @Test
-    void imposeLateFee() {
+    void getAllCurrentLoansByUserID_NonExistantUser() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+
+        List<Borrow> expectedBorrow = new ArrayList<>();
+        List<Borrow> currentActual = borrowDao.getAllCurrentLoansByUserID(333);
+;       assertEquals(expectedBorrow, currentActual);
     }
+
+    /**
+     * This tests getAllLoansByUserID
+     * where it is under normal conditions
+     */
+    @Test
+    void getAllLoansByUserID() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+        int user_id = 3;
+        int book_id = 2;
+        borrowDao.bookBorrow(user_id, book_id);
+
+        // Getting expected Borrowed Books
+        List<Borrow> actualBorrow = borrowDao.getAllLoansByUserID(user_id);
+
+        List<Borrow> expectedBorrow = new ArrayList<>();
+        expectedBorrow.add(new Borrow(1, user_id, book_id, LocalDate.now(), LocalDate.now().plusWeeks(4), null, 0));
+        // deleting the loan for the next test
+        borrowDao.deleteLoan(3,2);
+        assertEquals(expectedBorrow, actualBorrow);
+    }
+
+    /**
+     * This tests getAllLoansByUserID
+     * where it is under abnormal conditions,
+     * where the Userid does not exist
+     */
+    @Test
+    void getAllLoansByUserID2() throws DaoException {
+        BorrowDao borrowDao = new BorrowDao("testlibrary");
+
+        List<Borrow> actualBorrow = borrowDao.getAllLoansByUserID(222);
+
+        List<Borrow> expectedBorrow = new ArrayList<>();
+        assertEquals(expectedBorrow, actualBorrow);
+    }
+
+
 }
