05d4fc228a0f424db1f2481558a83b31426cb065
cgarvey00 <conorgarvey19@outlook.com>
2023-11-06 02:57:07 +0000

    PaymentDao Interface method updated

--- src/main/java/dao/PaymentDaoInterface.java
+++ src/main/java/dao/PaymentDaoInterface.java
@@ -1,7 +1,7 @@
 package dao;
 
-import business.Borrow;
 import business.Payment;
+import exceptions.DaoException;
 
 import java.util.List;
 
@@ -18,6 +18,18 @@ public interface PaymentDaoInterface {
      */
     List<Payment> getPaymentHistoryFromUserID(int user_id);
 
+    /**
+     * This method will the ensure the User makes their payment
+     * for any outstanding late fee for borrowing a book
+     *
+     * @param user_id     the users user_id
+     * @param book_id     the users book_id that they borrowed
+     * @param fine        the fine that has to be paid
+     * @param card_number the card number used to make the payment
+     * @param expiry_date the cards expiry date
+     * @return the updated row of the added payment
+     */
+    public int makePayment(int user_id, int book_id, int fine, String card_number, String expiry_date) throws DaoException;
 
 
 }
